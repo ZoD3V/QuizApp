9ca21ffa55415b32b33aa268b11828804c1f9bdf
lastorder <adzkiyadzikrullah@gmail.com>
2021-12-08 13:25:13 +0700

    First

---
+++ app/src/main/java/yayasan/idn/quizapp/Question.kt
@@ -0,0 +1,12 @@
+package yayasan.idn.quizapp
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
